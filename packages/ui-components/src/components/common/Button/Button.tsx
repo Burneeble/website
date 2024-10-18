b5a2e13559de1002267892df264ed7804cad5182
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-10-18 15:28:18 +0200

    Add styles to Button component and apply className in JSX

--- packages/ui-components/src/components/common/Button/Button.tsx
+++ packages/ui-components/src/components/common/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ButtonProps } from "./Button.types";
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.text}</button>;
+  return <button className="button">{props.text}</button>;
 };
 
 export default Button;
