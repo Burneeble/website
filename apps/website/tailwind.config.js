a58ca2199cc2a2d08528812e2f2569d2506bff26
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-10-19 12:51:51 +0200

    update

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [],
+  content: ["./src/**/*.{html,js,tsx,jsx}"],
   theme: {
     extend: {
       borderRadius: {
