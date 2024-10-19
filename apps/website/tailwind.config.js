ddb01221b186f05aeb07f18f4f3db8e7bd32e2b7
Chiara Motta <104596212+D0gg1@users.noreply.github.com>
2024-10-19 11:54:10 +0200

    Refactor Tailwind imports and file extensions

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: [],
   theme: {
+    screens: {
+      mobile: "425px",
+      tablet: "768px",
+      laptop: "992px",
+      desktop: "1300px",
+    },
+
+    colors: {},
+
     extend: {},
   },
   plugins: [],
-}
-
+};
