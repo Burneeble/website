28a593d71c31d7c3c6fbe46edf97e70318b48184
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-10-19 15:42:16 +0200

    fix tailwind in ui-components

---
+++ packages/ui-components/src/components/common/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { StoryObj, Meta } from "@storybook/react";
+import Button from "./Button";
+import { ButtonProps } from "./Button.types";
+
+export default {
+  title: "exomoon-components/common/Button",
+  component: Button,
+} as Meta<typeof Button>;
+
+type Story = StoryObj<ButtonProps>;
+
+export const primaryMedium: Story = {
+  args: {
+    text: "Button",
+  },
+};
--- packages/ui-components/src/components/common/Button/Button.tsx
+++ packages/ui-components/src/components/common/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ButtonProps } from "./Button.types";
 
 const Button = (props: ButtonProps) => {
-  return <button className="button">{props.text}</button>;
+  return <button className="button bg-red-400">{props.text}</button>;
 };
 
 export default Button;
