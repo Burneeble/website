6b2f1900d13dae03ba6d371a527e9a4633e2590d
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-10-21 09:26:17 +0200

    feat: add new UI components (Skeleton, Input, Separator, Tooltip) and update styles; refactor Tailwind configuration

---
+++ packages/ui-components/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./separator";
+export * from "./input";
+export * from "./sheet";
+export * from "./sidebar";
+export * from "./skeleton";
+export * from "./tooltip";
