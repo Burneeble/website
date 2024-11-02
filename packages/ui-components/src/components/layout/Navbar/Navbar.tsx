d5145f0820bb983cf1e25b92dfc74811767ed493
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-11-02 11:00:48 +0100

    chore: Make navbar customizable by props

--- packages/ui-components/src/components/layout/Navbar/Navbar.tsx
+++ packages/ui-components/src/components/layout/Navbar/Navbar.tsx
@@ -11,48 +11,8 @@ import {
   ListItem,
 } from "./../../ui/navigation-menu";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = (props: NavbarProps) => {
-  const components: { title: string; href: string; description: string }[] = [
-    {
-      title: "Alert Dialog",
-      href: "/docs/primitives/alert-dialog",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-      title: "Hover Card",
-      href: "/docs/primitives/hover-card",
-      description:
-        "For sighted users to preview content available behind a link.",
-    },
-    {
-      title: "Progress",
-      href: "/docs/primitives/progress",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-      title: "Scroll-area",
-      href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
-    },
-    {
-      title: "Tabs",
-      href: "/docs/primitives/tabs",
-      description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-      title: "Tooltip",
-      href: "/docs/primitives/tooltip",
-      description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-  ];
-
   return (
     <section>
       <nav className="tw-font-inter tw-mx-auto tw-h-auto tw-w-full tw-max-w-screen-2xl tw-flex ">
@@ -61,69 +21,57 @@ const Navbar = (props: NavbarProps) => {
         </div>
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="tw-grid tw-gap-3 tw-p-6 md:tw-w-[400px] lg:tw-w-[500px] lg:tw-grid-cols-[.75fr_1fr]">
-                  <li className="tw-row-span-3">
-                    <NavigationMenuLink asChild>
-                      <a
-                        className="tw-flex tw-h-full tw-w-full tw-select-none tw-flex-col tw-justify-end tw-rounded-md tw-bg-gradient-to-b tw-from-muted/50 tw-to-muted tw-p-6 tw-no-underline tw-outline-none focus:tw-shadow-md"
-                        href="/"
-                      >
-                        <FontAwesomeIcon
-                          icon={faReact}
-                          className="tw-h-6 tw-w-6"
-                        />
-                        <div className="tw-mb-2 tw-mt-4 tw-text-lg tw-font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="tw-text-sm tw-leading-tight tw-text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="tw-grid tw-w-[400px] tw-gap-3 tw-p-4 md:tw-w-[500px] md:tw-grid-cols-2 lg:tw-w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
+            {props.dropdowns.map((component, i) => {
+              return (
+                <NavigationMenuItem key={i}>
+                  <NavigationMenuTrigger>
+                    {component.title}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="tw-grid tw-gap-3 tw-p-6 md:tw-w-[400px] lg:tw-w-[500px] lg:tw-grid-cols-[.75fr_1fr]">
+                      {component.primaryItem && (
+                        <li className="tw-row-span-3">
+                          <NavigationMenuLink asChild>
+                            <a
+                              className="tw-flex tw-h-full tw-w-full tw-select-none tw-flex-col tw-justify-end tw-rounded-md tw-bg-gradient-to-b tw-from-muted/50 tw-to-muted tw-p-6 tw-no-underline tw-outline-none focus:tw-shadow-md"
+                              href={component.primaryItem.href}
+                            >
+                              {component.primaryItem.svg}
+                              <div className="tw-mb-2 tw-mt-4 tw-text-lg tw-font-medium">
+                                {component.primaryItem.title}
+                              </div>
+                              <p className="tw-text-sm tw-leading-tight tw-text-muted-foreground">
+                                {component.primaryItem.description}
+                              </p>
+                            </a>
+                          </NavigationMenuLink>
+                        </li>
+                      )}
+                      {component.items.map((item, j) => {
+                        return (
+                          <ListItem key={j} title={item.title} href={item.href}>
+                            {item.description}
+                          </ListItem>
+                        );
+                      })}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              );
+            })}
+            {props.links.map((link, i) => {
+              return (
+                <NavigationMenuItem key={i}>
+                  <Link href={link.href} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
                     >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Documentation
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+                      {link.title}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              );
+            })}
           </NavigationMenuList>
         </NavigationMenu>
       </nav>
--- packages/ui-components/src/components/layout/Navbar/Navbar.types.ts
+++ packages/ui-components/src/components/layout/Navbar/Navbar.types.ts
@@ -1,6 +1,25 @@
+export interface NavbarDropDownItem {
+  title: string;
+  href: string;
+  description: string;
+}
+
+export interface NavbarDropDown {
+  title: string;
+  primaryItem?: NavbarDropDownItem & { svg: JSX.Element };
+  items: NavbarDropDownItem[];
+}
+
+export interface NavbarLink {
+  title: string;
+  href: string;
+}
+
 export interface NavbarProps {
   logo: {
     svg: JSX.Element;
     url?: string;
   };
+  dropdowns: NavbarDropDown[];
+  links: NavbarLink[];
 }
