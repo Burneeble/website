7cbf1c03b6f7b9be7238d22914f3a3c9e44e2dfb
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-11-06 15:56:37 +0100

    feat: Add Customers component with custom scrollbars to Home page

--- apps/website/src/components/Pages/HomePage/Customers/Customers.tsx
+++ apps/website/src/components/Pages/HomePage/Customers/Customers.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import { CustomersProps } from "./Customers.types";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
 const Customers = (props: CustomersProps) => {
-  return <section className="customers"></section>;
+  return (
+    <section className="customers">
+      <Scrollbars autoHide={false} style={{ width: "100vw", height: "30rem" }}>
+        <div
+          className="tw-h-[10rem] tw-w-[200vw]"
+          style={{ border: "1px solid red" }}
+        ></div>
+      </Scrollbars>
+    </section>
+  );
 };
 
 export default Customers;
