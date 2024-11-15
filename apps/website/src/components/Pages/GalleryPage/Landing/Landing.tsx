89f2c8247f6bb017fa808fa297042e5b6c64d09f
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-11-15 11:38:14 +0100

    feat: Update Landing component with responsive background images and text sizes

--- apps/website/src/components/Pages/GalleryPage/Landing/Landing.tsx
+++ apps/website/src/components/Pages/GalleryPage/Landing/Landing.tsx
@@ -11,18 +11,24 @@ const Landing = (props: LandingProps) => {
   return (
     <section
       className={`
-        cs-section-structure tw-h-[622px] tw-flex tw-justify-center
-        tw-items-center
+        tw-h-[622px] tw-flex tw-justify-center tw-items-center
         tw-bg-[linear-gradient(180deg,_black_0%,_rgba(102,102,102,0)_100%),url('/img/gallery-page/landing/bg-sm.png')]
         tw-bg-no-repeat tw-bg-cover tw-bg-center
 
         md:tw-bg-[linear-gradient(180deg,_black_0%,_rgba(102,102,102,0)_100%),url('/img/gallery-page/landing/bg-md.png')]
+
+        xl:tw-bg-[linear-gradient(180deg,_black_0%,_rgba(102,102,102,0)_100%),url('/img/gallery-page/landing/bg-xl.png')]
+        xl:tw-h-[764px]
       `}
     >
       <div
         className={`
           tw-h-[151px] tw-flex-col tw-justify-center tw-items-center tw-gap-2.5
           tw-flex tw-text-5xl tw-font-bowlby-one
+
+          md:tw-text-[65px]
+
+          xl:tw-text-[80px]
         `}
       >
         <div className="tw-font-[60px]">
@@ -39,9 +45,13 @@ const Landing = (props: LandingProps) => {
           className={`
             tw-text-center tw-text-headings tw-text-3xl tw-font-normal
             tw-relative -tw-top-1/4
+
+            md:tw-text-5xl md:-tw-top-[30%]
+
+            xl:tw-text-[60px] xl:-tw-top-[40%]
           `}
         >
-          Showcase
+          {["md", "sm"].includes(screen) ? "Showcase" : "Discover our Showcase"}
         </div>
       </div>
     </section>
