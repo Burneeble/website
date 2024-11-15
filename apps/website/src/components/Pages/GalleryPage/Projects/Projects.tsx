83b51ba639a62a43df1147eb0d3be87a849e0566
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-11-15 14:47:28 +0100

    feat: Implement project fetching and display in Projects component with updated ProjectPreview styles

--- apps/website/src/components/Pages/GalleryPage/Projects/Projects.tsx
+++ apps/website/src/components/Pages/GalleryPage/Projects/Projects.tsx
@@ -1,8 +1,35 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ProjectsProps } from "./Projects.types";
 import { faFilter, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { ProjectModel, useProjectService } from "@/services/ProjectService";
+import { useEffect, useState } from "react";
+import { NotificationHandler, ProjectPreview } from "@burneeble/ui-components";
 
 const Projects = (props: ProjectsProps) => {
+  //States
+  const [projects, setProjects] = useState<Array<ProjectModel> | null>(null);
+
+  //Hooks
+  const { getProjects } = useProjectService();
+
+  //Effects
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  //Methods
+  const fetchProjects = async () => {
+    try {
+      const res = await getProjects();
+      setProjects(res);
+    } catch (e) {
+      console.log(e);
+      NotificationHandler.instance.error("Error fetching projects");
+    }
+  };
+
   return (
     <section
       className={`
@@ -20,7 +47,12 @@ const Projects = (props: ProjectsProps) => {
           tw-opacity-[.7] tw-blur-[100px]
         `}
       />
-      <div className={`content cs-section-structure tw-relative tw-z-[2]`}>
+      <div
+        className={`
+          content cs-section-structure tw-relative tw-z-[2] tw-flex tw-flex-col
+          tw-gap-[20px]
+        `}
+      >
         <div
           className={`
             header tw-h-[58px] tw-justify-between tw-items-center tw-inline-flex
@@ -47,6 +79,17 @@ const Projects = (props: ProjectsProps) => {
             </div>
           </div>
         </div>
+        {projects &&
+          projects.map((project, i) => {
+            return (
+              <ProjectPreview
+                key={i}
+                thumbnail={project.thumbnailUrl}
+                title={project.title}
+                categories={project.categories}
+              />
+            );
+          })}
       </div>
     </section>
   );
--- packages/ui-components/src/components/common/ProjectPreview/ProjectPreview.tsx
+++ packages/ui-components/src/components/common/ProjectPreview/ProjectPreview.tsx
@@ -5,27 +5,37 @@ const ProjectPreview = (props: ProjectPreviewProps) => {
   return (
     <div
       className={`
-        tw-inline-flex tw-w-full tw-flex-col tw-items-start tw-justify-start
-        tw-gap-[10px] tw-rounded-lg
+        tw-group tw-inline-flex tw-w-full tw-cursor-pointer tw-flex-col
+        tw-items-start tw-justify-start tw-gap-[10px] tw-rounded-lg
       `}
     >
-      <img
-        className={`
-          tw-aspect-[1920/1080] tw-w-full tw-self-stretch tw-rounded-tl-lg
-          tw-rounded-tr-lg tw-border tw-border-[var(--neutral-default)]
-        `}
-        src={props.thumbnail}
-      />
-
       <div
         className={`
-          tw-flex tw-h-[60px] tw-flex-col tw-items-start tw-justify-center
+          image-wrapper tw-aspect-[1920/1080] tw-w-full tw-overflow-hidden
+          tw-rounded-tl-lg tw-rounded-tr-lg tw-border
+          tw-border-[var(--neutral-default)]
+        `}
+      >
+        <img
+          className={`
+            tw-aspect-[1920/1080] tw-w-full tw-self-stretch tw-transition-all
+            tw-duration-200 tw-ease-in-out
+
+            group-hover:tw-scale-[1.1]
+          `}
+          src={props.thumbnail}
+        />
+      </div>
+      <div
+        className={`
+          project-info tw-flex tw-h-[60px] tw-max-w-full tw-flex-col
+          tw-items-start tw-justify-center
         `}
       >
         <div
           className={`
-            tw-font-inter tw-text-xl tw-font-black tw-leading-loose
-            tw-text-headings
+            title tw-max-w-full tw-truncate tw-font-inter tw-text-xl
+            tw-font-black tw-leading-loose tw-text-headings
 
             xl:tw-text-2xl
           `}
@@ -34,8 +44,8 @@ const ProjectPreview = (props: ProjectPreviewProps) => {
         </div>
         <div
           className={`
-            tw-self-stretch tw-font-inter tw-text-lg tw-font-normal tw-leading-7
-            tw-text-headings
+            categories tw-max-w-full tw-self-stretch tw-truncate tw-font-inter
+            tw-text-lg tw-font-normal tw-leading-7 tw-text-headings
 
             xl:tw-text-xl
           `}
