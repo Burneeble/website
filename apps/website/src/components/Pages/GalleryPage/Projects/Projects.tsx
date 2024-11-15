c996c2d4eb6f8212ba4d05830316bf7f9ef903be
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-11-15 12:19:46 +0100

    feat: Add header and icon functionality to Projects component with updated styles

--- apps/website/src/components/Pages/GalleryPage/Projects/Projects.tsx
+++ apps/website/src/components/Pages/GalleryPage/Projects/Projects.tsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ProjectsProps } from "./Projects.types";
+import { faFilter, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = (props: ProjectsProps) => {
   return (
@@ -18,7 +20,34 @@ const Projects = (props: ProjectsProps) => {
           tw-opacity-[.7] tw-blur-[100px]
         `}
       />
-      <div className={`content cs-section-structure`}></div>
+      <div className={`content cs-section-structure tw-relative tw-z-[2]`}>
+        <div
+          className={`
+            header tw-h-[58px] tw-justify-between tw-items-center tw-inline-flex
+            tw-w-full tw-gap-[20px]
+          `}
+        >
+          <div
+            className={`
+              title tw-text-white tw-text-2xl tw-font-normal tw-font-bowlby-one
+            `}
+          >
+            GALLERY
+          </div>
+          <div
+            className={`
+              icons tw-justify-end tw-items-center tw-gap-[5px] tw-flex
+            `}
+          >
+            <div className={`icon`}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </div>
+            <div className={`icon`}>
+              <FontAwesomeIcon icon={faFilter} />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
