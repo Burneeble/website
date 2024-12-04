0c8af1167862e30ef196571b76d53fe62dac80f2
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-12-04 12:51:30 +0100

    feat: Enhance Skill component with triangle separator and conditional rendering logic

--- apps/website/src/components/Pages/HomePage/Abilities/components/SkillsParallax/components/Skill/Skill.tsx
+++ apps/website/src/components/Pages/HomePage/Abilities/components/SkillsParallax/components/Skill/Skill.tsx
@@ -37,35 +37,62 @@ const Skill = (props: SkillProps) => {
             tw-relative
           `}
         >
-          <FontAwesomeIcon
-            icon={faFire}
-            className={`
-              fontawesome-gradient-icon tw-text-[42px] tw-relative tw-z-[2]
-              tw-filter tw-drop-shadow-[0px_0px_1.5px_rgba(0,0,0,0.5)]
-            `}
-          />
+          {(
+            !["sm", "md"].includes(screen)
+              ? props.currentIndex >= props.index
+              : true
+          ) ? (
+            <FontAwesomeIcon
+              icon={faFire}
+              className={`
+                fontawesome-gradient-icon tw-text-[42px] tw-relative tw-z-[2]
+                tw-filter tw-drop-shadow-[0px_0px_1.5px_rgba(0,0,0,0.5)]
+              `}
+            />
+          ) : (
+            <div
+              className={`
+                triangle tw-bg-gradient-to-t tw-from-[#2B2B2B] tw-to-[#73503D]
+                tw-w-[42px] tw-h-[50px] tw-relative tw-z-[2]
+              `}
+            />
+          )}
           <div
             className={cn(
               `
                 rod tw-absolute tw-bottom-[-15px] tw-left-1/2
                 -tw-translate-x-1/2 tw-h-[calc(100%-11px)] tw-w-[8px]
-                tw-bg-gradient-to-b tw-from-[var(--primary-default)]
-                tw-to-[var(--primary-lighest)]
 
                 xl:tw-h-[calc(100%-20px)]
               `,
               props.index == props.amount - 1 &&
-                "xl:tw-h-[calc(100%-40px)] xl:tw-bottom-0"
+                "xl:tw-h-[calc(100%-40px)] xl:tw-bottom-0",
+              (
+                !["sm", "md"].includes(screen)
+                  ? props.currentIndex >= props.index
+                  : true
+              )
+                ? `
+                  tw-bg-gradient-to-b tw-from-[var(--primary-default)]
+                  tw-to-[var(--primary-lighest)]
+                `
+                : `tw-bg-[var(--secondary-darker)]`
             )}
           />
         </div>
       )}
       <div
-        className={`
-          info tw-flex tw-flex-col tw-gap-[10px]
+        className={cn(
+          `
+            info tw-flex tw-flex-col tw-gap-[10px]
 
-          xl:tw-pb-[30px]
-        `}
+            xl:tw-pb-[30px] xl:tw-transition-all xl:tw-duration-200
+            xl:tw-ease-in-out
+          `,
+          !["sm", "md"].includes(screen) &&
+            props.currentIndex < props.index &&
+            "tw-blur-[5px]"
+        )}
       >
         <h3
           className={`
