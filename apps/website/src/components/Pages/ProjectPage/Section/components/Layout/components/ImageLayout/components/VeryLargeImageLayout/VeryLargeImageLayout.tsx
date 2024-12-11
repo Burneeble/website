3af6b1978a22ea694a92422a5ee2eaaf12a6597f
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-12-11 09:11:35 +0100

    feat: Update LargeImageLayout and VeryLargeImageLayout components for improved height and responsiveness

--- apps/website/src/components/Pages/ProjectPage/Section/components/Layout/components/ImageLayout/components/VeryLargeImageLayout/VeryLargeImageLayout.tsx
+++ apps/website/src/components/Pages/ProjectPage/Section/components/Layout/components/ImageLayout/components/VeryLargeImageLayout/VeryLargeImageLayout.tsx
@@ -3,11 +3,15 @@ import { VeryLargeImageLayoutProps } from "./VeryLargeImageLayout.types";
 const VeryLargeImageLayout = (props: VeryLargeImageLayoutProps) => {
   return (
     <div
-      className={`images-layout very-large-image-layout tw-relative tw-w-full`}
+      className={`
+        images-layout very-large-image-layout tw-relative tw-w-full tw-h-[410px]
+
+        md:tw-h-[600px]
+      `}
     >
       <img
         src={props.image1}
-        className={`tw-object-cover tw-aspect-[1300/600] tw-w-full`}
+        className={`tw-object-cover tw-h-full tw-w-full`}
       />
     </div>
   );
