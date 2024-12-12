9ce4202d1fdb701c364c752e2e1f0dedd2111373
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-12-12 09:11:57 +0100

    feat: Refactor ThreeImagesLayout component for improved responsiveness and layout consistency

--- apps/website/src/components/Pages/ProjectPage/Section/components/Layout/components/ImageLayout/components/ThreeImagesLayout/ThreeImagesLayout.tsx
+++ apps/website/src/components/Pages/ProjectPage/Section/components/Layout/components/ImageLayout/components/ThreeImagesLayout/ThreeImagesLayout.tsx
@@ -4,45 +4,46 @@ const ThreeImagesLayout = (props: ThreeImagesLayoutProps) => {
   return (
     <div
       className={`
-        images-layout three-images-layout tw-relative tw-w-[365px] tw-h-[370px]
+        images-layout three-images-layout tw-relative tw-w-full tw-h-auto
+        tw-aspect-[365/370] tw-max-w-[50vw]
 
-        md:tw-w-[710px] md:tw-h-[440px]
+        md:tw-aspect-[710/440]
 
-        xl:tw-w-[1110px] xl:tw-h-[555px]
+        xl:tw-aspect-[1110/555]
       `}
     >
       <img
         src={props.image1}
         className={`
           tw-object-cover tw-aspect-[720/450] tw-absolute tw-top-0 tw-z-[1]
-          tw-w-[365px] tw-left-0
+          tw-w-full tw-left-0
 
-          md:tw-w-[540px] md:tw-left-[140px]
+          md:tw-w-[76%] md:tw-left-[19.7%]
 
-          xl:tw-w-[720px] xl:tw-left-0
+          xl:tw-w-[64.8%] xl:tw-left-0
         `}
       />
       <img
         src={props.image2}
         className={`
-          tw-object-cover tw-aspect-[600/450] tw-absolute tw-z-[2] tw-w-[230px]
-          tw-rotate-[-12deg] tw-top-[100px] tw-left-[25px]
+          tw-object-cover tw-aspect-[600/450] tw-absolute tw-z-[2] tw-w-[63%]
+          tw-rotate-[-12deg] tw-top-[27%] tw-left-[6.8%]
 
-          md:tw-w-[450px] md:tw-bottom-0 md:tw-left-0 md:tw-right-[unset]
+          md:tw-w-[63%] md:tw-bottom-0 md:tw-left-0 md:tw-right-[unset]
           md:tw-rotate-0 md:tw-top-[unset]
 
-          xl:tw-left-[unset] xl:tw-w-[600px] xl:tw-bottom-0 xl:tw-right-[72px]
+          xl:tw-left-[unset] xl:tw-w-[54%] xl:tw-bottom-0 xl:tw-right-[13%]
         `}
       />
       <img
         src={props.image3}
         className={`
-          tw-object-cover tw-aspect-[205/440] tw-absolute tw-z-[3] tw-w-[80px]
-          tw-right-[75px] tw-bottom-0
+          tw-object-cover tw-aspect-[205/440] tw-absolute tw-z-[3] tw-w-[22%]
+          tw-right-[20.5%] tw-bottom-0
 
-          md:tw-w-[155px] md:tw-right-0 md:tw-bottom-0 md:tw-rotate-[11deg]
+          md:tw-w-[16.2%] md:tw-right-0 md:tw-bottom-0 md:tw-rotate-[11deg]
 
-          xl:tw-rotate-0 xl:tw-w-[205px] xl:tw-top-0
+          xl:tw-rotate-0 xl:tw-w-[18.4%] xl:tw-top-0
         `}
       />
     </div>
