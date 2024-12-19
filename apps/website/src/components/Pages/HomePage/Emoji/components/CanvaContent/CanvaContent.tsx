0477eac2897c4941586e5acaac68b9ffa63d431c
Chiara Motta <104596212+D0gg1@users.noreply.github.com>
2024-12-19 17:09:37 +0100

    feat: Update CanvaContent to use matcap texture and add floating torus geometry; enhance visual dynamics

--- apps/website/src/components/Pages/HomePage/Emoji/components/CanvaContent/CanvaContent.tsx
+++ apps/website/src/components/Pages/HomePage/Emoji/components/CanvaContent/CanvaContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as THREE from "three";
+import { useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import {
   OrbitControls,
@@ -11,41 +12,36 @@ import {
 } from "@react-three/drei";
 import { useRef } from "react";
 
+const torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 100);
+const material = new THREE.MeshMatcapMaterial();
+
 const CanvaContent = () => {
-  const cubeRef = useRef<THREE.Mesh>(null);
   const groupRef = useRef<THREE.Group>(null);
 
   // E9CCA1_A63106_DF8C3B_621304
   // 422509_C89536_824512_0A0604
   const [matcapTexture] = useMatcapTexture("E9CCA1_A63106_DF8C3B_621304", 512);
 
+  //update the material with the new texture
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
   useFrame((state, delta) => {
-    // Rotate the mesh every frame, this is outside of React without overhead
-    if (cubeRef.current)
-      cubeRef.current.rotation.x = cubeRef.current.rotation.y += delta;
+    if (groupRef.current)
+      for (const child of groupRef.current.children as THREE.Mesh[]) {
+        child.rotation.y += delta * 0.2;
+      }
   });
 
   return (
     <>
       <OrbitControls enableZoom={false} />
 
-      <group ref={groupRef} position-y={2}>
-        <mesh ref={cubeRef}>
-          <boxGeometry args={[1.5, 1.5, 1.5]} />
-          <meshBasicMaterial color={"purple"} />
-        </mesh>
-        <mesh position-x={-2}>
-          <sphereGeometry args={[1, 32, 32]} />
-          <meshBasicMaterial color="orange" />
-        </mesh>
-      </group>
-      {/* fontSize={0.5}
-        color={"white"}
-        textAlign="center" */}
-
       <Center>
-        {/* <Float floatIntensity={5}>         </Float>
-         */}
         <Text3D
           font={"/Bowlby One_Regular.json"}
           size={0.5}
@@ -53,14 +49,32 @@ const CanvaContent = () => {
           curveSegments={12}
           bevelEnabled
           bevelThickness={0.02}
-          bevelSize={0.02}
+          bevelSize={0.01}
           bevelOffset={0}
           bevelSegments={5}
+          material={material}
         >
-          <meshMatcapMaterial matcap={matcapTexture} />
           Still not sure?
         </Text3D>
       </Center>
+      <Float>
+        <group ref={groupRef}>
+          {[...Array(20)].map((_, index) => (
+            <mesh
+              geometry={torusGeometry}
+              material={material}
+              key={index}
+              position={[
+                Math.random() * 10 - 5,
+                Math.random() * 10 - 5,
+                Math.random() * 10 - 5,
+              ]}
+              scale={0.4 + Math.random() * 0.4}
+              rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+            />
+          ))}
+        </group>
+      </Float>
     </>
   );
 };
