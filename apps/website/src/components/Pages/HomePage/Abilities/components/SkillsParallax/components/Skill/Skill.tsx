7d3957dafe9f04775aaf2d1029e3d80c51056e04
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2025-01-03 10:43:45 +0100

    feat: Simplify icon animation logic and enhance responsive design in Skill component

--- apps/website/src/components/Pages/HomePage/Abilities/components/SkillsParallax/components/Skill/Skill.tsx
+++ apps/website/src/components/Pages/HomePage/Abilities/components/SkillsParallax/components/Skill/Skill.tsx
@@ -33,16 +33,8 @@ const Skill = (props: SkillProps) => {
   //Effects
   useEffect(() => {
     if (iconRef.current) {
-      const iconPath =
-        "M159.3 5.4c7.8-7.3 19.9-7.2 27.7 .1c27.6 25.9 53.5 53.8 77.7 84c11-14.4 23.5-30.1 37-42.9c7.9-7.4 20.1-7.4 28 .1c34.6 33 63.9 76.6 84.5 118c20.3 40.8 33.8 82.5 33.8 111.9C448 404.2 348.2 512 224 512C98.4 512 0 404.1 0 276.5c0-38.4 17.8-85.3 45.4-131.7C73.3 97.7 112.7 48.6 159.3 5.4zM225.7 416c25.3 0 47.7-7 68.8-21c42.1-29.4 53.4-88.2 28.1-134.4c-4.5-9-16-9.6-22.5-2l-25.2 29.3c-6.6 7.6-18.5 7.4-24.7-.5c-16.5-21-46-58.5-62.8-79.8c-6.3-8-18.3-8.1-24.7-.1c-33.8 42.5-50.8 69.3-50.8 99.4C112 375.4 162.6 416 225.7 416z";
-      const trianglePath = "M224 32 L32 480 L416 480 Z";
-
-      const targetPath =
-        props.currentIndex >= props.index ? iconPath : trianglePath;
-
       gsap.to(iconRef.current, {
         duration: 0.5,
-        attr: { d: targetPath },
         fill:
           props.currentIndex >= props.index
             ? 'url("#gradient")'
@@ -66,7 +58,7 @@ const Skill = (props: SkillProps) => {
 
     if (divs) {
       if (["sm", "md", "lg"].includes(screen)) {
-        Array.from(divs).forEach((div, index) => {
+        Array.from(divs).forEach((div) => {
           gsap.to(div, {
             scale: 1,
             opacity: 1,
@@ -232,12 +224,14 @@ const Skill = (props: SkillProps) => {
           className={cn(
             `
               categories tw-flex tw-flex-wrap tw-gap-[10px] tw-overflow-hidden
-              tw-transition-all tw-duration-500 tw-relative
+              tw-transition-all tw-duration-500 tw-relative tw-max-h-[16rem]
 
               after:tw-content-[''] after:tw-absolute after:tw-left-0
               after:tw-w-full after:tw-bg-gradient-to-b
               after:tw-from-[rgba(0,0,0,0)] after:tw-bottom-0 after:tw-block
-              after:tw-to-[black] after:tw-duration-250 after:tw-transition-all tw-max-h-[16rem] xl:tw-max-h-[0]
+              after:tw-to-[black] after:tw-duration-250 after:tw-transition-all
+
+              xl:tw-max-h-[0]
             `,
 
             !["sm", "md", "lg"].includes(screen) && showShadow
